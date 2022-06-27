ab0f49686505368e9ccdbe9d80cef2a5e95dda07
Maksat Ylyasov <maxintelinc@gmail.com>
2022-06-27 11:08:49 +0000

    after first lab completed

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,15 +15,22 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def index_sample(request):
+    context={}
+    return render(request, 'djangoapp/index_sample.html', context)
 
 # Create an `about` view to render a static about page
 # def about(request):
 # ...
-
+def about(request):
+    context={}
+    return render(request, 'djangoapp/about.html', context)
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
+def contact(request):
+    context={}
+    return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost",
+"maksat219_djangoapp.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
